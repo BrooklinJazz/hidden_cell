16ee60e5f4e60c8f9df3c01611fbaf1c1f6b9382
Brooklin <brooklinjazzmyers@gmail.com>
2022-08-18 15:17:35 -0700

    add title input

--- lib/hidden_cell.ex
+++ lib/hidden_cell.ex
@@ -6,23 +6,29 @@ defmodule HiddenCell do
   @impl true
   def init(attrs, ctx) do
     source = attrs["source"] || ""
-    {:ok, assign(ctx, source: source)}
+    title = attrs["title"] || "Hidden Cell"
+    {:ok, assign(ctx, source: source, title: title)}
   end
 
   @impl true
   def handle_connect(ctx) do
-    {:ok, %{source: ctx.assigns.source}, ctx}
+    {:ok, %{source: ctx.assigns.source, title: ctx.assigns.title}, ctx}
   end
 
   @impl true
-  def handle_event("update", %{"source" => source}, ctx) do
-    broadcast_event(ctx, "update", %{"source" => source})
+  def handle_event("update_source", %{"source" => source}, ctx) do
+    broadcast_event(ctx, "update_source", %{"source" => source})
     {:noreply, assign(ctx, source: source)}
+  end
+
+  def handle_event("update_title", %{"title" => title}, ctx) do
+    broadcast_event(ctx, "update_title", %{"title" => title})
+    {:noreply, assign(ctx, title: title)}
   end
 
   @impl true
   def to_attrs(ctx) do
-    %{"source" => ctx.assigns.source}
+    %{"source" => ctx.assigns.source, "title" => ctx.assigns.title}
   end
 
   @impl true
@@ -37,32 +43,48 @@ defmodule HiddenCell do
 
       ctx.root.innerHTML = `
         <section id="container">
-          <p id="title">Hidden Cell</p>
+          <p id="title">${payload.title}</p>
+          <input id="title_input"></input>
           <textarea id="source"></textarea>
         </section>
       `;
 
       const textarea = ctx.root.querySelector("#source");
       const title = ctx.root.querySelector("#title");
+      const title_input = ctx.root.querySelector("#title_input");
       const container = ctx.root.querySelector("#container");
 
       textarea.style.display = "none"
       textarea.value = payload.source;
 
+      title_input.style.display = "none"
+      title_input.value = payload.title;
+
       textarea.addEventListener("change", (event) => {
-        ctx.pushEvent("update", { source: event.target.value });
+        ctx.pushEvent("update_source", { source: event.target.value });
+      });
+
+      title_input.addEventListener("change", (event) => {
+        ctx.pushEvent("update_title", { title: event.target.value });
       });
 
       title.addEventListener("dblclick", (event) => {
         if (textarea.style.display == "none") {
           textarea.style.display = "block"
+          title_input.style.display = "block"
         } else {
           textarea.style.display = "none"
+          title_input.style.display = "none"
         }
       });
 
-      ctx.handleEvent("update", ({ source }) => {
+      ctx.handleEvent("update_source", ({ source }) => {
         textarea.value = source;
+      });
+
+      ctx.handleEvent("update_title", ({ title: title_text }) => {
+        title.innerHTML = title_text;
+        title_input.value = title_text;
       });
 
       ctx.handleSync(() => {
@@ -77,8 +99,6 @@ defmodule HiddenCell do
   asset "main.css" do
     """
     #source {
-      box-sizing: border-box;
-      width: 100%;
       min-height: 100px;
     }
 
@@ -91,6 +111,11 @@ defmodule HiddenCell do
       font-family: Inter, system-ui,-apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
     }
 
+    #title_input, #source {
+      width: 100%;
+      margin: 0.5rem 0;
+      box-sizing: border-box;
+    }
     #title {
       text-align: center;
       cursor: pointer;
